44f36036dc60f7b0d6734f2c07cbbbcf1d0f0eea
adxxya30 <adityaofficial714@gmail.com>
2025-10-28 20:58:07 +0700

    feat(dashboard): early design dashboard page

--- apps/web/src/app/(protect)/dashboard/page.tsx
+++ apps/web/src/app/(protect)/dashboard/page.tsx
@@ -1,25 +1,78 @@
-import { headers } from "next/headers";
+import { Edit3, Link2, Share2 } from "lucide-react";
+import Link from "next/link";
 import { redirect } from "next/navigation";
-import { authClient } from "@/lib/auth-client";
-import Dashboard from "./dashboard";
+import { Card, CardContent } from "@/components/ui/card";
+import { getSession } from "@/lib/auth-server";
 
 export default async function DashboardPage() {
-	const session = await authClient.getSession({
-		fetchOptions: {
-			headers: await headers(),
-			throw: true,
-		},
-	});
+	const session = await getSession();
 
 	if (!session?.user) {
 		redirect("/login");
 	}
 
 	return (
-		<div>
-			<h1>Dashboard</h1>
-			<p>Welcome {session.user.name}</p>
-			<Dashboard session={session} />
-		</div>
+		<main className="container mx-auto max-w-5xl px-4 py-8">
+			<h1 className="mb-8 font-bold text-3xl">Welcome {session?.user.name}</h1>
+			<div className="grid grid-cols-2 gap-8">
+				<EditLink />
+				<SocialLinks />
+				<ShareLink />
+			</div>
+		</main>
 	);
 }
+
+const EditLink = () => {
+	return (
+		<Link href="/dashboard/profile">
+			<Card className="border bg-card shadow-md transition-all duration-300 hover:shadow-lg">
+				<CardContent className="space-y-4 pt-2 pb-4 text-center">
+					<div className="mb-2 inline-flex h-16 w-16 items-center justify-center rounded-2xl bg-accent/10 text-primary">
+						<Link2 className="h-8 w-8" />
+					</div>
+					<h3 className="font-semibold text-xl">Manage Profile</h3>
+					<p className="text-muted-foreground leading-relaxed">
+						Manage your Title, Subtitle, Username and other details
+					</p>
+				</CardContent>
+			</Card>
+		</Link>
+	);
+};
+
+const SocialLinks = () => {
+	return (
+		<Link href="/dashboard/links">
+			<Card className="border bg-card shadow-md transition-all duration-300 hover:shadow-lg">
+				<CardContent className="space-y-4 pt-2 pb-4 text-center">
+					<div className="mb-2 inline-flex h-16 w-16 items-center justify-center rounded-2xl bg-accent/10 text-primary">
+						<Edit3 className="h-8 w-8" />
+					</div>
+					<h3 className="font-semibold text-xl">Edit Link</h3>
+					<p className="text-muted-foreground leading-relaxed">
+						Add, edit, or remove link in popular social platforms
+					</p>
+				</CardContent>
+			</Card>
+		</Link>
+	);
+};
+
+const ShareLink = () => {
+	return (
+		<div>
+			<Card className="border bg-card shadow-md transition-all duration-300 hover:shadow-lg">
+				<CardContent className="space-y-4 pt-2 pb-4 text-center">
+					<div className="mb-2 inline-flex h-16 w-16 items-center justify-center rounded-2xl bg-accent/10 text-primary">
+						<Share2 className="h-8 w-8" />
+					</div>
+					<h3 className="font-semibold text-xl">Share Link</h3>
+					<p className="text-muted-foreground leading-relaxed">
+						Share your link with your friends
+					</p>
+				</CardContent>
+			</Card>
+		</div>
+	);
+};
