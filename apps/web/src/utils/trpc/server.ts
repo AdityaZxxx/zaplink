98944fe5f3ba3a8e71337d55aa73024e3fa5fcd9
adxxya30 <adityaofficial714@gmail.com>
2025-10-29 14:44:52 +0700

    feat(trpc): add server utils and reorganize folder

---
+++ apps/web/src/utils/trpc/server.ts
@@ -0,0 +1,19 @@
+import { createContext } from "@zaplink/api/context";
+import { appRouter } from "@zaplink/api/routers/index";
+import { headers } from "next/headers";
+
+export async function trpcServer() {
+	const nextHeaders = headers();
+	const headerMap = new Headers(await nextHeaders);
+
+	const ctx = await createContext({
+		context: {
+			// @ts-expect-error
+			request: {
+				headers: headerMap,
+			},
+		},
+	});
+
+	return appRouter.createCaller(ctx);
+}
