480a27727c33dac93992e33d93bd9e741f37f0e3
adxxya30 <adityaofficial714@gmail.com>
2025-12-01 11:31:56 +0700

    feat: use neon serverless driver

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,20 +1,18 @@
+import { Pool } from "@neondatabase/serverless";
 import dotenv from "dotenv";
+import { drizzle } from "drizzle-orm/neon-serverless";
+import * as authSchema from "./schema/auth";
+import * as linkSchema from "./schema/link";
+import * as profileSchema from "./schema/profile";
 
 dotenv.config({
 	path: "../../apps/server/.env",
 });
 
-import { neon, neonConfig } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-http";
-import ws from "ws";
+const schema = { ...authSchema, ...linkSchema, ...profileSchema };
 
-neonConfig.webSocketConstructor = ws;
-
-// To work in edge environments (Cloudflare Workers, Vercel Edge, etc.), enable querying over fetch
-// neonConfig.poolQueryViaFetch = true
-
-const sql = neon(process.env.DATABASE_URL || "");
-export const db = drizzle(sql);
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+export const db = drizzle(pool, { schema });
 
 export * from "drizzle-orm";
 
