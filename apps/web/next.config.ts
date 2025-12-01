393c53ab2d3d1888b7e7b3a1b55a57562a1f820d
adxxya30 <adityaofficial714@gmail.com>
2025-12-01 20:53:42 +0700

    feat: configure image domains

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -3,6 +3,20 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 	typedRoutes: true,
 	reactCompiler: true,
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "ql0mzp860t.ufs.sh",
+				pathname: "/**",
+			},
+			{
+				protocol: "https",
+				hostname: "utfs.io",
+				pathname: "/**",
+			},
+		],
+	},
 };
 
 export default nextConfig;
