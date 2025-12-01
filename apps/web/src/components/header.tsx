43c50065625dcf56c0537348e95e32315437a040
adxxya30 <adityaofficial714@gmail.com>
2025-12-01 11:36:46 +0700

    feat: update header layout

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -1,18 +1,17 @@
 "use client";
+
 import Link from "next/link";
-import { ModeToggle } from "./mode-toggle";
 import UserMenu from "./user-menu";
 
 export default function Header() {
 	return (
-		<nav className="container mx-auto flex h-16 max-w-5xl flex-row items-center justify-between border-b px-2 py-1">
+		<nav className="container mx-auto flex h-16 max-w-7xl flex-row items-center justify-between border-b px-2 py-1">
 			<div className="flex items-center">
 				<Link href="/" className="flex items-center">
 					<span className="font-bold text-2xl tracking-tight">Zaplink</span>
 				</Link>
 			</div>
 			<div className="flex items-center gap-4">
-				<ModeToggle />
 				<UserMenu />
 			</div>
 		</nav>
