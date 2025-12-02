fa5ac501d00d96e18d5e5d3222cff66009635518
adxxya30 <adityaofficial714@gmail.com>
2025-12-02 12:56:40 +0700

    feat: add analytics schema to package

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,6 +1,7 @@
 import { Pool } from "@neondatabase/serverless";
 import dotenv from "dotenv";
 import { drizzle } from "drizzle-orm/neon-serverless";
+import * as analyticsSchema from "./schema/analytic";
 import * as authSchema from "./schema/auth";
 import * as linkSchema from "./schema/link";
 import * as profileSchema from "./schema/profile";
@@ -9,13 +10,18 @@ dotenv.config({
 	path: "../../apps/server/.env",
 });
 
-const schema = { ...authSchema, ...linkSchema, ...profileSchema };
+const schema = {
+	...authSchema,
+	...linkSchema,
+	...profileSchema,
+	...analyticsSchema,
+};
 
 const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 export const db = drizzle(pool, { schema });
 
 export * from "drizzle-orm";
-
+export * from "./schema/analytic";
 export * from "./schema/auth";
 export * from "./schema/link";
 export * from "./schema/profile";
