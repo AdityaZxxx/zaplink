f1d0e3b99c86781afc0262f6d9f5d33ecce8480a
adxxya30 <adityaofficial714@gmail.com>
2025-12-03 07:46:18 +0700

    refactor: remove header component

--- apps/web/src/app/(protect)/layout.tsx
+++ apps/web/src/app/(protect)/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import Header from "@/components/header";
 import Providers from "@/components/providers";
 import "../../index.css";
 
@@ -29,10 +28,7 @@ export default function RootLayout({
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 			>
-				<Providers>
-					<Header />
-					{children}
-				</Providers>
+				<Providers>{children}</Providers>
 			</body>
 		</html>
 	);
