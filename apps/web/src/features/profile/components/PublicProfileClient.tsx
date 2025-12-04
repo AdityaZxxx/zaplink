c3ebb093a9fa70ba8c1dba8d24fc51e2dc80efc4
adxxya30 <adityaofficial714@gmail.com>
2025-12-04 12:25:33 +0700

    feat: create public profile client component

---
+++ apps/web/src/features/profile/components/PublicProfileClient.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { useMutation } from "@tanstack/react-query";
+import type { links, profiles } from "@zaplink/db";
+import { useEffect } from "react";
+import ProfileCard from "@/components/ProfileCard";
+import { trpc } from "@/utils/trpc/client";
+
+type Profile = typeof profiles.$inferSelect;
+type Link = typeof links.$inferSelect;
+
+interface PublicProfileClientProps {
+	profile: Profile;
+	links: Link[];
+}
+
+export default function PublicProfileClient({
+	profile,
+	links,
+}: PublicProfileClientProps) {
+	const trackViewMutation = useMutation(
+		trpc.analytics.trackView.mutationOptions(),
+	);
+	const trackClickMutation = useMutation(
+		trpc.analytics.trackClick.mutationOptions(),
+	);
+
+	useEffect(() => {
+		if (profile.username) {
+			trackViewMutation.mutate({ username: profile.username });
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [profile.username]);
+
+	const handleLinkClick = (linkId: number) => {
+		trackClickMutation.mutate({ linkId });
+	};
+
+	return (
+		<ProfileCard
+			profile={profile}
+			links={links}
+			onLinkClick={handleLinkClick}
+		/>
+	);
+}
